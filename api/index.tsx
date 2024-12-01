531fc29706f386430f3059406205804c113b1df3
Jacek <jacek@degen.tips>
2024-12-01 17:39:26 +0100

    Add isClaimed handling

--- api/index.tsx
+++ api/index.tsx
@@ -3,6 +3,7 @@ import { devtools } from 'frog/dev';
 // import { neynar } from 'frog/hubs'
 import { handle } from 'frog/next';
 import { serveStatic } from 'frog/serve-static';
+import { ethers } from 'ethers';
 import { abi } from './abi.js';
 
 // Uncomment to use Edge Runtime.
@@ -57,6 +58,31 @@ app.frame('/', (c) => {
     intents: [<Button.Transaction target="/claim">Claim</Button.Transaction>],
   });
 });
+
+const BASE_RPC_URL = 'https://mainnet.base.org';
+const CONTRACT_ADDRESS = '0x16EB07225C95FBec33BcB40f1a69c61F5A9aa6D5';
+
+async function getIsClaimed(merkleIndex: any) {
+  try {
+    // Connect to the Base network provider
+    const provider = new ethers.JsonRpcProvider(BASE_RPC_URL, {
+      name: 'Base Mainnet',
+      chainId: 8453,
+    });
+
+    // Create an instance of the contract
+    const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
+
+    // Call the `isClaimed` function
+    const isClaimed = await contract.isClaimed(merkleIndex.toString());
+
+    console.log(`isClaimed for index ${merkleIndex}:`, isClaimed);
+    return isClaimed;
+  } catch (error) {
+    console.error('Error fetching isClaimed status:', error.message);
+    throw error;
+  }
+}
 
 app.frame('/finish', (c) => {
   // const { transactionId } = c || { transactionId: 'None.' };
@@ -114,14 +140,13 @@ app.transaction('/claim', async (c) => {
 
     const data = await response.json();
 
-    // Check if the response contains data
     if (!data || !data.length) {
       throw new Error(
         'Oops! This wallet isn’t linked to an airdrop. Try another one?'
       );
     }
 
-    // Extract the required parameters
+    // Extract Merkle proof data
     const {
       index: merkleIndex,
       wallet_address: merkleWallet,
@@ -129,13 +154,21 @@ app.transaction('/claim', async (c) => {
       proof: merkleProof,
     } = data[0];
 
+    const isClaimed = await getIsClaimed(merkleIndex);
+
+    if (isClaimed) {
+      throw new Error(
+        'Oops! It looks like you’ve already claimed this airdrop!'
+      );
+    }
+
     // Send the transaction
     return c.contract({
       abi,
       chainId: 'eip155:8453', // Replace with the appropriate chain ID
-      functionName: 'claim', // Replace with the actual function name
+      functionName: 'claim',
       args: [merkleIndex, merkleWallet, merkleAmount, merkleProof],
-      to: '0x16EB07225C95FBec33BcB40f1a69c61F5A9aa6D5', // Replace with the actual contract address
+      to: CONTRACT_ADDRESS,
     });
   } catch (error) {
     return c.error({ message: (error as Error).message });
